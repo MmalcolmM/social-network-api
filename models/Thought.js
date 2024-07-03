672534346b88e5f571510e6418dd69da41e881ef
Malcolm Franklin <mac.mac5@yahoo.com>
2024-07-02 18:16:07 -0600

    updated models

--- models/Thought.js
+++ models/Thought.js
@@ -1,31 +1,35 @@
 const { Schema, model } =  require('mongoose');
+const reactionSchema = require('./Reaction')
 
 const thoughtSchema = new Schema(
     {
         thoughtText: {
             type: String,
             required: true,
-            min_length: 1,
-            max_length: 280,
+            minlength: 1,
+            maxlength: 280,
         },
         createdAt: {
             type: Date,
             default: Date.now(),
+            get: (timestamp) => Date(timestamo).toLocaleString(),
         },
         username: {
             type: String,
             required: true,
         },
         reactions: [reactionSchema],
+    },
+    {
+        toJSON: {
+            getters: true,
+            virtuals: true,
+        },
+        id: false,
     }
 );
 
-thoughtSchema.virtual('formattedCreatedAt').get(function() {
-    const options = { month: '2-digit', day: '2-digit', year: 'numeric' };
-    return this.createdAt.toLocaleDateString('en-US', options);
-  });
-
-  thoughtSchema.virtual('reactioncount').get(function () {
+  thoughtSchema.virtual('reactionCount').get(function () {
     return this.reactions.legnth;
   });
 
